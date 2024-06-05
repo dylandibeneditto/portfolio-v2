3a3f7710560bee4b68206a5e0d774fb384c28d3a
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-06-04 23:16:43 -0400

    fix: box title again

--- components/BoxTitle.tsx
+++ components/BoxTitle.tsx
@@ -3,7 +3,7 @@ import "./BoxTitle.css";
 export default function BoxTitle(props: {title: string}) {
     return (
         <div>
-            <div className="page-title"> \/\/ <span className="a">{props.title}</span></div>
+            <div className="page-title"> {"//"} <span className="a">{props.title}</span></div>
         </div>
     )
 }
